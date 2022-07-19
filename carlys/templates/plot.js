1e2ffd8de2fbb71d169e85f5d4b918eb1e994320
Carly-Jane <101085083+Carly-Jane@users.noreply.github.com>
2022-07-19 20:32:52 +0800

    working through plotly visualisations and updating webscraping file

--- carlys/templates/plot.js
+++ carlys/templates/plot.js
@@ -29,7 +29,66 @@
 // Income Table Titled Aus_Income
 // Aus_Income Column Headers:
 // # State	
-// # Median Income	
+// # Median_Income	
+
+// VISUALISATIONS:
+
+// Which state has the highest population? Which state has the lowest?
+// Using table Aus_Population, columns State & Total
+// Trace Population Data
+d3.json("data/data.json").then(incomingData)
+
+var trace = {
+    x: data.map(row => row.State),
+    y: data.map(row => row.Total),
+    text: data.map(row => row.State),
+    name: "State Populations",
+    type: "bar"
+};
+
+// Apply the group barmode to the layout
+var layout = {
+    title: "State Populations",
+};
+
+// Render the plot to the div tag with id "plot"
+Plotly.newPlot("plot", trace, layout);
+
+// Which state has the highest median income? Which state has the lowest?
+// Using table Aus_Income, columns State & Median_Income	
+d3.json("data/data.json").then(incomingData)
+
+var trace = {
+    x: data.map(row => row.State),
+    y: data.map(row => row.Median_Income),
+    text: data.map(row => row.State),
+    name: "State Incomes",
+    type: "bar"
+};
+
+// Apply the group barmode to the layout
+var layout = {
+    title: "State Incomes",
+};
+
+// Render the plot to the div tag with id "plot"
+Plotly.newPlot("plot", trace, layout);
+
+// Which state produces the most energy per capita? which state produces the least? 
+// Divided into renewable vs non renewable.
+
+
+// Do states with a higher population have a higher percentage of renewable energy
+// production? State population vs the % of energy productions that is renewable.
+
+
+// Do states with a higher median income have a higher percentage of renewable energy
+// production? State median income vs the % of energy productions that is renewable.
+
+
+// Show each state renewables growth. Ie, a double bar graph, showing the amount
+// of renewable plants in WA in 2013 next to the amount of renewable plants in WA in 2020
+
 
 
 // Incoming data is internally referred to as incomingData
@@ -73,3 +132,35 @@ d3.json("data/data.json").then((incomingData) => {
     // Plot the chart to a div tag with id "bar-plot"
     Plotly.newPlot("bar-plot", data, layout);
 });
+
+
+
+// Trace1 for the Greek Data
+var trace1 = {
+    x: data.map(row => row.pair),
+    y: data.map(row => row.greekSearchResults),
+    text: data.map(row => row.greekName),
+    name: "Greek",
+    type: "bar"
+};
+
+// Trace 2 for the Roman Data
+var trace2 = {
+    x: data.map(row => row.pair),
+    y: data.map(row => row.romanSearchResults),
+    text: data.map(row => row.romanName),
+    name: "Roman",
+    type: "bar"
+};
+
+// Combining both traces
+var traceData = [trace1, trace2];
+
+// Apply the group barmode to the layout
+var layout = {
+    title: "Greek vs Roman gods search results",
+    barmode: "group"
+};
+
+// Render the plot to the div tag with id "plot"
+Plotly.newPlot("plot", traceData, layout);
