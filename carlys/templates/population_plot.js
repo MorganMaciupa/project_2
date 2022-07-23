51ef0f9c0e69764a5e19ae782c6a95a6b3181c20
Morgan Maciupa <morganmaciupa@hotmail.com>
2022-07-23 20:58:28 +0800

    draft plots

--- carlys/templates/population_plot.js
+++ carlys/templates/population_plot.js
@@ -1,11 +1,12 @@
-var source = '../JSON_data/Aus_Population.json'
+var pop_source = '../JSON_data/Aus_Population.json'
+var income_source = '../JSON_data/Aus_Income.json'
 
-function buildPlots(state) {
-    d3.json(source).then(function (data) {
-       console.log(data.map(row => row.State));
+function buildPopPlot(state) {
+    d3.json(pop_source).then(function (data) {
+      // console.log(data.map(object => object.State));
 
-       var state = data.map(row => row.State)
-       var totals = data.map(row => row.Total)
+       var state = data.map(object => object.State)
+       var totals = data.map(object => object.Total)
 
         // Build the bar chart//
         var trace = {
@@ -16,18 +17,47 @@ function buildPlots(state) {
             name: "State Populations",
         };
 
-        // Apply the group barmode to the layout
-        var layout = {
-            title: "State Populations",
-};
+        var plot_data = [trace];
 
         // Apply the group barmode to the layout
         var layout = {
             title: "State Populations",
+            bargap: 0.5,
 };
 
-        Plotly.newPlot("plot", trace, layout);
+        Plotly.newPlot("plot", plot_data, layout);
     })
 };
 
-buildPlots(source)
+buildPopPlot(pop_source)
+
+function buildIncomePlot(state) {
+    d3.json(income_source).then(function (data) {
+      // console.log(data.map(object => object.State));
+
+       var state = data.map(object => object.State)
+       var income = data.map(object => object.Median_Income)
+
+        // Build the bar chart//
+        var trace = {
+            type: 'bar',
+            x: state,
+            y: income,
+            text: state,
+            name: "Median Income per State",
+        };
+
+        var plot_data = [trace];
+
+        // Apply the group barmode to the layout
+        var layout = {
+            title: "Median Income per State",
+            bargap: 0.5,
+};
+
+        Plotly.newPlot("income_plot", plot_data, layout);
+    })
+};
+
+buildPopPlot(pop_source)
+buildIncomePlot(income_source)
