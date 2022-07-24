bd49fc903fc6247b4f24df37fd2e129ff040923a
Morgan Maciupa <morganmaciupa@hotmail.com>
2022-07-25 04:52:26 +0800

    finalised jupyter

--- carlys/templates/population_plot.js
+++ carlys/templates/population_plot.js
@@ -1,5 +1,6 @@
 var pop_source = '../JSON_data/Aus_Population.json'
 var income_source = '../JSON_data/Aus_Income.json'
+var per_capita_source = '../JSON_data/energy_production_per_capita'
 
 function buildPopPlot(state) {
     d3.json(pop_source).then(function (data) {
@@ -59,5 +60,45 @@ function buildIncomePlot(state) {
     })
 };
 
+
+
+function buildPerCapita(state) {
+    d3.json(per_capita_source).then(function (data) {
+      console.log(state);
+
+       var state = data.State;
+       var renewable = data.Renewable_MWh_Per_Capita;
+       var non_renewable = data.Non_Renewable_MWh_Per_Capita;
+
+        // Build the bar chart//
+        var trace_renewable = {
+            type: 'bar',
+            x: state,
+            y: renewable,
+            text: state,
+            name: "Renewable Energy per Capita",
+        };
+
+        // var trace_nonrenewable = {
+           // type: 'bar'
+            // x: state,
+            // y: non_renewable
+           // text: state,
+            //name: "Non-Renewable Energy per Capita",
+        // }
+
+        var plot_data = [trace_renewable];
+
+        // Apply the group barmode to the layout
+        var layout = {
+            title: "Energy Production per Capita",
+            bargap: 0.5,
+};
+
+        Plotly.newPlot("per_capita_plot", plot_data, layout);
+    })
+};
+
 buildPopPlot(pop_source)
 buildIncomePlot(income_source)
+buildPerCapita(per_capita_source)
